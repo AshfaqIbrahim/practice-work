2cc8eae37d9d7295e3b1e0d8356acd095e05dc07
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2025-10-29 19:45:19 +0530

    updated

--- Worksheet/script.js
+++ Worksheet/script.js
@@ -990,6 +990,8 @@
 // double(6,2,display); 
 
 
+////****************************************************************************************************************
+
 
 // //Promises in JS
 // let prm = new Promise((resolve, reject) =>{
@@ -1005,6 +1007,8 @@
 // .catch((result) => {
 //     console.log(result);
 // })
+
+
 
 
 // //
@@ -1024,5 +1028,112 @@
 // .catch((result) => {
 //     console.log(result);
 // })
+
+
+// //async and await
+// function prm(){
+//     return new Promise((resolve,reject) =>{
+//       setTimeout(() => {
+//         console.log("Task completed");  
+//         reject();      
+//       },3000) 
+//     }) 
+// }
+
+// async function run(){
+//   try {
+//     await prm();
+//     console.log("Promise fulfilled");
+//   }
+//   catch(err){
+//     console.log("Promise not fulfilled!");
+    
+//   }
+// }
+// run();
+
+
+
+// let prm = new Promise((resolve,reject) => {
+//   console.log("Successful executor function");
+//   reject("Task not completed")  
+// })
+
+// prm
+// .then((result) => {
+//   console.log(result)
+// })
+// .catch((result) => {
+//   console.log(result);
+// })
+
+
+// //Promise chain using async and await
+// function task1(){
+//   return new Promise((resolve,reject) => {
+//     console.log("Task 1 started")
+    
+//     setTimeout(() => {
+//       console.log("Task 1 completed!");
+//       resolve();
+//     },3000)
+//   })
+// }
+
+// function task2(){
+//   return new Promise((resolve,reject) => {
+//     console.log("Task 2 started");
+   
+//    setTimeout(() => {
+//     console.log("Task 2 completed!");
+//     resolve();
+//    },3000)
+//   })
+// }
+
+// function task3(){
+//   return new Promise((resolve,reject) => {
+//     console.log("Task 3 started");
+  
+//     setTimeout(() => {
+//       console.log("Task 3 completed");
+//       resolve();
+//     },3000)
+//   })
+// }
+
+// function task4(){
+//   return new Promise((resolve,reject) => {
+//     console.log("Task 4 started");
+  
+//     setTimeout(() => {
+//       console.log("Task 4 completed");
+//       resolve();
+//     },3000)
+//   })
+// }
+
+
+// //using await and async but without funciton we dont want async. here i havce used try and catch for chained promise 
+// try{
+//   await task1();
+//   await task2();
+//   await task3();
+//   await task4();
+//   console.log("All tasks completed...");
+  
+// }
+
+// catch(err){
+//   console.log("Promise failed");
+// }
+
+
+// task1()
+// .then(task2)
+// .then(task3)
+// .then(task4)
+// .then(() => console.log("All tasks are completed!"))
+// .catch(() => console.log("Promise failed"))
 
 
